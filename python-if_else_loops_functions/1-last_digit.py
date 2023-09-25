d1381b235ea3edb3ccc5173b16285e836030e013
mickaeltartar <mickael.tartar@hotmail.com>
2023-09-25 16:16:00 +0200

    add statement for positive & negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10, 10)
+number = random.randint(-10000, 10000)
 
-digit = number % 10 if number >= 0 else -(-number % 10)
-print("Last digit of", number, "is", digit, end=" ")
+if number < 0:
+    digit = -number % 10 * -1
+else:
+    digit = number % 10
 
 if digit > 5:
-    print("and is greater than 5")
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, digit))
 elif digit == 0:
-    print("and is 0")
+    print("Last digit of {:d} is {:d} and is 0".format(number, digit))
 else:
-    print("and is less than 6 and not 0")
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, digit))
