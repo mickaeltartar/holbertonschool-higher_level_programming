9b484c1216885fcb11bb726e6d5fcba4161f54da
mickaeltartar <mickael.tartar@hotmail.com>
2023-09-26 11:09:24 +0200

    fix indent for print in statement

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ separator = ", "
 for number in range(0, 100):
     if number == 99:
         separator = "\n"
-print("{:02d}".format(number), end=separator)
+    print("{:02d}".format(number), end=separator)
