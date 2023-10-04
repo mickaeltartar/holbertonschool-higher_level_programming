04cf813701c1447128352889ddcc6791c1dcccce
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-04 17:29:46 +0200

    delete parenthese

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
-        return (0)
+        return 0
     sum = 0
     nums = {
         "I": 1,
@@ -15,7 +15,7 @@ def roman_to_int(roman_string):
         "M": 1000
     }
     for i in range(len(roman_string)):
-        value = nums[roman_string[index]]
+        value = nums[roman_string[i]]
         if i + 1 < len(roman_string) and nums[roman_string[i + 1]] > value:
             sum -= value
         else:
