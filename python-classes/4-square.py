67e77d6d981c60b954e9d0b735b973845ad2213a
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-09 15:50:40 +0200

    define a square with property

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""module 0
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """ init """
+        self.size = size
+
+    @property
+    def size(self):
+        """ getter method to retreive """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ setter method to initialize value """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ return the current area """
+        return (self.__size ** 2)
