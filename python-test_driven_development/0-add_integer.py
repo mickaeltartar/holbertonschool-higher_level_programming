0384e083c512ff23624f24b472652f38111ac17f
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-10 10:46:42 +0200

    add program that result the sum of two int & add test files

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+function that add two numbers
+"""
+
+
+def add_integer(a, b=98):
+
+    """_summary_
+
+    Args:
+        a (int): number
+        b (int, optional): number set defaults to 98.
+
+    Raises:
+        TypeError: if a or b is not a int
+
+    Returns:
+        int: result a + b
+    """
+    if isinstance(a, (int, float)):
+        a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+    if isinstance(a, (int, float)):
+        a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+    return a + b
