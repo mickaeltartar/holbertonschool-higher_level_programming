2e86fc18d725c8fda38a2895cb0ab4cf1324126c
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-12 13:10:22 +0200

    add property & setter for the class rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" create a rectangle """
+
+
+class Rectangle:
+    """ start the class """
+    def __init__(self, width=0, height=0):
+        """ init rectangle
+
+        Args:
+            width: rectangle width
+            height: rectangle height
+
+        raises:
+            TypeError: if width or height is not integer
+            ValueError: if width or height is < 0
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ define width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ define value of width """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ define height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ define value of height """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__height = value
