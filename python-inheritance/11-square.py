77cf35107981abb86bec21167e6ed3d2d20108c5
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-17 16:32:42 +0200

    print the size of the square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Square that inherits from Rectangle """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class square """
+
+    def __init__(self, size):
+        """ init a square """
+
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """ retunr area of the square """
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """ print the size for the square """
+
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
