e3b10d9dfb5e7ee60f5ad21b29f6b18c0d014c94
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-17 13:12:41 +0200

    add space

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """ return class if is instance or not """
 
-    return isinstance(obj, a_class) and not issubclass(a_class,obj.__class__)
+    return isinstance(obj, a_class) and not issubclass(a_class, obj.__class__)
