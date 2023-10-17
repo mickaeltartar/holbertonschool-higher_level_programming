d8662853bb81efe19278a47bb7431d2be9db35b7
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-17 16:06:21 +0200

    print the width & the height from the rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+""" a class rectangle that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ create a rectangle """
+
+    def __init__(self, width, height):
+
+        """ define width and height in private instance """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        """ print the width & the height from the rectangle """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
