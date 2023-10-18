a840768f7a8e7a9696e0c17d45eb0deded767df4
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-18 11:40:58 +0200

    write a function that read a file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" read file """
+
+def read_file(filename=""):
+    """ reading file function """
+    with open(filename, encoding="utf-8") as f:
+            print(f.read(), end="")
