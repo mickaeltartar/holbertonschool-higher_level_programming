53ddcdcbe30309c94ad24f5609064153e49f1709
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-18 12:27:24 +0200

    write the function that write & create a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" write file """
+
+
+def write_file(filename="", text=""):
+    """ writing the file function """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
