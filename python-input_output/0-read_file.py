c11ac9033c351bc7262640db7849cfd97d376db7
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-18 12:08:38 +0200

    pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ read file """
 
+
 def read_file(filename=""):
     """ reading file function """
     with open(filename, encoding="utf-8") as f:
-            print(f.read(), end="")
+        print(f.read(), end="")
