e7c994213b94162b62eaa0a5e36a8389e9809618
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-19 17:55:07 +0200

    add blank

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
+""" Write a class Student """
 
-class student():
+
+class Student():
+    """ class student """
+
     def __init__(self, first_name, last_name, age):
         """ Args:
                 first_name(str): the first name
                 last_name(str): the last name
                 age(int): age
         """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """ retrieves a dictionary representation of a Student instance """
+
         if attrs is None:
             return self.__dict__
 
