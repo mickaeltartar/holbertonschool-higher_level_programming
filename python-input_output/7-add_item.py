5937dba94990f72a4351e97f62e3be41f27e0d0f
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-19 13:45:11 +0200

    add permission

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,20 +5,21 @@
 import sys
 
 
-save_to_jason_file = __import__('5-save_to_jason_file').save_to_jason_file
-load_from_jason_file = __import__('6-load_from_jason_file').load_from_jason_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_items_to_jason():
     """ add items """
 
     try:
-        item_list = load_from_jason_file('add_item_json')
+        item_list = load_from_json_file('add_item.json')
+
     except FileNotFoundError:
         item_list = []
 
     item_list.extend(sys.argv[1:])
-    save_to_jason_file(item_list, "add_item_json")
+    save_to_json_file(item_list, "add_item.json")
 
 
 if __name__ == "__main__":
