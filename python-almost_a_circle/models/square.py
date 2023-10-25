505994cc6bcce5a839ad442587a939be20b10842
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-25 13:54:20 +0200

    add width & height value in setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,7 +8,8 @@ class Square(Rectangle):
     """ class square """
     def __init__(self, size, x=0, y=0, id=None):
         """ init square method
-        args: size: square size
+        args:
+            size: square size
             x: x position
             y: y position
             id: object id
@@ -21,3 +22,19 @@ class Square(Rectangle):
         """
         return ("[Square] ({}) {:d}/{:d} - {:d}".format(self.id, self.x,
                                                         self.y, self.width))
+
+    @property
+    def size(self):
+        """ width getter method
+        return: size of width
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ width & height setter method
+        args:
+        value: size value
+        """
+        self.width = value
+        self.height = value
