0feab2cfb8a5e482f39e758735128eeb82736e1b
mickaeltartar <mickael.tartar@hotmail.com>
2023-10-25 14:55:58 +0200

    add class method for find and write object in dictionnary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -46,3 +46,17 @@ class Base:
         return: list of JSON string
         """
         return json.dumps(list_dictionaries or [])
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ write JSON string in file
+        args:
+        list_objs: list of object
+        """
+        if list_objs:
+            index = cls.to_json_string([obj.to_dictionnary()for
+                                        obj in list_objs])
+        else:
+            index = '[]'
+        with open(cls.__name__ + '.json', 'w') as file:
+            file.write(index)
