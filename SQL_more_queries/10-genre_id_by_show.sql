bcaa61b4ec931481e25d5b083814e5d2d5dcc655
mickaeltartar <mickael.tartar@hotmail.com>
2023-11-22 14:16:25 +0100

    list all show contained in database

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+
+SELECT tv_show.title, tv_show_genres.genre_id
+FROM tv_show
+JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+ORDER BY tv_show.title ASC, tv_show_genres.genre_id ASC;
