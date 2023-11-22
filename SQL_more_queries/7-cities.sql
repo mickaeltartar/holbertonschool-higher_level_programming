452a016b967d45058dd6f8543e9b8eef02896a4e
mickaeltartar <mickael.tartar@hotmail.com>
2023-11-22 13:49:21 +0100

    add comment & add foreign key at the same line of state id

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,9 +6,10 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
+-- create table & set it
+
 CREATE TABLE IF NOT EXISTS cities(
 id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-state_id INT NOT NULL,
+state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES state(id),
 name VARCHAR(256) NOT NULL,
-FOREIGN KEY (state_id) REFERENCES state(id)
 );
