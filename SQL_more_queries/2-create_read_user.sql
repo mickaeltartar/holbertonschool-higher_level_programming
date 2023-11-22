1c6c20a7476a7e11e93c4510f64a7b16f4103c03
mickaeltartar <mickael.tartar@hotmail.com>
2023-11-22 13:11:30 +0100

    create database & user & add provilege to read

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- create new database & new user with read privilege
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- create user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- privileges for user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
